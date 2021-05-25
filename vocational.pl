c6a64e9fb6c88ca910d5095af04d574ddd687e95
Paulo Sergio <psdsfj.snf20@uea.edu.br>
2021-05-25 19:07:53 -0400

    #Feature: import aswers to main scope and get user input and answer

--- vocational.pl
+++ vocational.pl
@@ -1,6 +1,14 @@
+init:-
+	consult("answers.rules").
+
 vocational:-
     write('Olá, sou o Vocational! Como posso ajudar a escolher seu futuro?'),nl,
+    init,
     repeat,
-    readln(Sentence),
-    write(Sentence). 
-  
+	format('-- '),
+	readln(Sentence),
+	answersrelatedtosentence(Sentence, Answer),
+	validateAnswer(Answer).
+
+validateAnswer(Answer):-
+	format('> '), format(Answer), nl, fail.
